55b614951e73d0c78f127ff8d545aa0c126b33a5
Damian Smallwood <wazsmallwood@gmail.com>
2020-01-27 19:03:33 +0200

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Opticron.Classes;
+
+namespace Opticron
+{
+    public class IndexModel : PageModel
+    {
+        public Product newProduct;
+        public Event latestEvent;
+        public News latestNews;
+        public Gallery gallery;
+        public List<Offer> specialOffers;
+        public List<Category> categories;
+        public void OnGet()
+        {
+            newProduct = (new Product()).SelectProduct()[0];
+            latestEvent = (new Event()).SelectAllEvents()[0];
+            latestNews = (new News()).SelectAllNews()[0];
+            gallery = (new Gallery()).SelectAllGallery()[0];
+            specialOffers = (new Offer()).SelectAllOffers();
+            categories = (new Category()).SelectAllCategoies();
+            
+
+
+        }
+    }
+}
